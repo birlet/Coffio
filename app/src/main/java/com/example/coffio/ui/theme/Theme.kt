76568ff670da275c387aed39b7ffd6e65c2b6504
birlet <adam.birlet@gmail.com>
2026-03-17 18:52:57 +0100

    change color scheme to white only

--- app/src/main/java/com/example/coffio/ui/theme/Theme.kt
+++ app/src/main/java/com/example/coffio/ui/theme/Theme.kt
@@ -9,35 +9,32 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green80,
+    secondary = GreenGrey80,
+    tertiary = Brown80,
+    onBackground = DarkGreen,
+    onSurface = DarkGreen
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green40,
+    secondary = GreenGrey40,
+    tertiary = Brown40,
+    background = Color.White,
+    surface = Color.White,
+    onBackground = DarkGreen,
+    onSurface = DarkGreen,
+    onSurfaceVariant = DarkGreen.copy(alpha = 0.7f)
 )
 
 @Composable
 fun CoffioTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    darkTheme: Boolean = false,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
